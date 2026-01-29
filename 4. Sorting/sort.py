445cb08add8452fbc0d43ea111e170a77c82e7f8
Anant <acera52024@gmail.com>
2026-01-29 13:53:01 +0530

    sort

--- 4. Sorting/sort.py
+++ 4. Sorting/sort.py
@@ -12,9 +12,44 @@ def bubble_sort(list):
 
                 # n^2
 
-    
-list = [5,4,3,1,2]
+def selectionSort(arr):
+    n = len(arr)
 
-print(list)
-bubble_sort(list)
-print(list)
+    for i in range(n-1):
+        min_index = i
+
+        for j in range(i+1,n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+
+        temp = arr[i]
+        arr[i] = arr[min_index]
+        arr[min_index]=temp
+
+def insertionSort(arr):
+    n = len(arr)
+
+    for i in range(1,n):
+        key = arr[i]
+        j = i-1
+
+        while j>=0 and arr[j]>key:
+            arr[j+1] = arr[j]
+            j = j-1
+            
+        arr[j+1] = key
+
+    return arr
+
+
+list = [5,4,3,1,2]
+arr = [5,4,3,1,2]
+
+# print(list)
+# bubble_sort(list)
+# print(list)
+
+
+print(arr)
+selectionSort(arr)
+print(arr)
